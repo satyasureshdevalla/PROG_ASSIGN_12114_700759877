01fec64da61f511fd4d05639543597bb7030296e
satyasureshdevalla <devallasatyasuresh@gmail.com>
2023-09-20 22:15:49 -0500

    fix: update domain object

--- src/main/java/edu/ucmo/Netflix.java
+++ src/main/java/edu/ucmo/Netflix.java
@@ -7,10 +7,12 @@ import java.util.List;
 @Document(collection = "netflix")
 public class Netflix {
     @Id
-    private String id;
+    private String _id;
     private String age_certification;
     private String description;
     private List<String> genres;
+
+    private String id;
     private List<String> production_countries;
     private Integer release_year;
     private Integer runtime;
@@ -21,12 +23,25 @@ public class Netflix {
 
     }
 
-    public String getId() {
-        return id;
+    public Netflix(String _id, String age_certification, String description, List<String> genres, String id, List<String> production_countries, Integer release_year, Integer runtime, String title, String type) {
+        this._id = _id;
+        this.age_certification = age_certification;
+        this.description = description;
+        this.genres = genres;
+        this.id = id;
+        this.production_countries = production_countries;
+        this.release_year = release_year;
+        this.runtime = runtime;
+        this.title = title;
+        this.type = type;
     }
 
-    public void setId(String id) {
-        this.id = id;
+    public String get_id() {
+        return _id;
+    }
+
+    public void set_id(String _id) {
+        this._id = _id;
     }
 
     public String getAge_certification() {
@@ -51,6 +66,14 @@ public class Netflix {
 
     public void setGenres(List<String> genres) {
         this.genres = genres;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
 
     public List<String> getProduction_countries() {
